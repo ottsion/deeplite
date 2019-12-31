bce568af6c9d104e389533991c0fd09e32c01762
ottsion <ottsion@163.com>
2019-12-31 17:42:15 +0800

    init

---
+++ data_loader/data_loaders.py
@@ -0,0 +1,24 @@
+from torchvision import datasets, transforms
+from base import BaseDataLoader
+from data_loader.DataSet_builder import *
+
+
+class MnistDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class CriteoDataLoader(BaseDataLoader):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1):
+        self.data_dir = data_dir
+        self.dataset = CriteoDataset(self.data_dir)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
---
+++ model/layers.py
@@ -0,0 +1,51 @@
+import torch as torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class FeaturesLinear(nn.Module):
+
+    def __init__(self, field_dims, output_dim=1):
+        super(FeaturesLinear, self).__init__()
+        self.fc = nn.Embedding(sum(field_dims), output_dim)
+        self.bias = nn.Parameter(torch.zeros((output_dim,)))
+        # 按照所给定的轴参数返回元素的梯形累计和，axis=0，按照行累加。axis=1，按照列累加。axis不给定具体值，就把numpy数组当成一个一维数组。
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+
+    def forward(self, x):
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return torch.add(torch.sum(self.fc(x), dim=1), self.bias)
+
+
+class FeaturesEmbedding(nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super(FeaturesEmbedding, self).__init__()
+        self.embedding = nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return self.embedding(x)
+
+
+class FactorizationMachine(nn.Module):
+    def __init__(self, reduce_sum=True):
+        super(FactorizationMachine, self).__init__()
+        self.reduce_sum = reduce_sum
+
+    def forward(self, x):
+        """
+             $\frac{1}{2}\sum_{k=1}^{K}[(\sum_{i=1}^{n}v_{ik}x_i)^2-\sum_{i=1}^{n}v_{ik}^2x_i^2]$
+
+        :param x: float tensor of size (batch_size, num_fields, embed_dim)
+        :return:
+        """
+        square_of_sum = torch.sum(x, dim=1) ** 2
+        sum_of_square = torch.sum(x ** 2, dim=1)
+        ix = square_of_sum - sum_of_square
+        if self.reduce_sum:
+            ix = torch.sum(ix, dim=1, keepdim=True)
+        return 0.5 * ix
